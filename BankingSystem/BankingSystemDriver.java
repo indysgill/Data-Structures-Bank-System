76df4ce645cc2d242987e64007efe94c9630579f
indysgill <160431697+indysgill@users.noreply.github.com>
2024-02-18 16:22:30 -0800

    Update BankingSystemDriver.java

--- BankingSystem/BankingSystemDriver.java
+++ BankingSystem/BankingSystemDriver.java
@@ -2,19 +2,11 @@ package BankingSystem;
 
 public class BankingSystemDriver {
     public static void main(String[] args) {
-//        Bank bank = new Bank();
+       // Bank bank = new Bank();
 
-        SavingsAccount sa = new SavingsAccount(1000);
-        CurrentAccount ca = new CurrentAccount(-500);
+        SavingsAccount sa = new SavingsAccount(1000, "Sa");
+        CurrentAccount ca = new CurrentAccount(-500, "Ca");
 
-        System.out.println("List of accounts: "+Bank.printToString());
-        // test cases for Current Account
-        ca.deposit(100);
-        System.out.println("Current account balance: "+ca.getBalance());
-        ca.withdraw(100);
-        System.out.println("Current account balance after withdrawal: "+ca.getBalance());
-        System.out.println("Total Balance in Bank: " + Bank.calculateTotalBalance());
-        Bank.deleteAccount(ca);
-        System.out.println("After deleting account, total balance in bank: "+Bank.calculateTotalBalance());
+        System.out.println("Names of Accounts: " + Bank.printToString());
     }
 }
